6be26459047b2284ade3b7979efde497f2ca2d8c
unknown <jobs.cluna@gmail.com>
2021-12-27 18:03:38 -0500

    fix(server): import and set libraries <@devCluna>

--- server/server.js
+++ server/server.js
@@ -1,1 +1,22 @@
-console.log("Hello World")
+const express = require('express')
+const dotenv = require('dotenv')
+const cors = require('cors')
+const SpotifyWebApi = require('spotify-web-api-node')
+
+dotenv.config()
+const app = express()
+
+//Middlewares
+app.use(cors())
+app.use(express.json())
+app.use(express.urlencoded({
+    extended: true
+}));
+
+
+
+
+app.listen(process.env.PORT, ()=>{
+    console.clear()
+    console.log("Server runnning on port:", process.env.PORT)
+})
