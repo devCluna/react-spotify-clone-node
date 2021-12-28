a3136204ea1455c00f2af33dbd1499251ecc6d94
Cristhian Luna <jobs.cluna@gmail.com>
2021-12-27 20:03:12 -0500

    feat(client): create Login file & authentication link <@devCluna>

---
+++ client/src/pages/Login.js
@@ -0,0 +1,12 @@
+import { AUTH_URL } from "../../lib/spotify"
+
+const Login = () => {
+
+    return (
+        <div>
+           <a href={AUTH_URL}>Login with Spotify</a> 
+        </div>
+    )
+}
+
+export default Login
