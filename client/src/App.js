580b4f0b8a0cd3011c359089e39c9aca10ca35b2
Cristhian Luna <jobs.cluna@gmail.com>
2021-12-27 20:50:30 -0500

    feat(app): create app component <@devCluna>

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,28 @@
+import {useEffect, useState} from 'react'
 import './App.css';
+import Dashboard from './pages/Dashboard/Dashboard';
+import Login from './pages/Login/Login';
+import axios from 'axios'
+
+const codeSearch = new URLSearchParams(window.location.search).get('code')
+axios.defaults.baseURL= process.env.REACT_APP_SERVER_URL
 
 function App() {
-  return (
-    <div>
-      
+  useEffect(()=>{
+     
+  }, [])
+  const [code, setCode] = useState()
+  
+useEffect(()=>{
+  setCode(codeSearch)
+},[codeSearch])
+
+  return(
+    <div className='App'> 
+    
+    {code ? <Dashboard code={code} setCode={setCode}/> : <Login/>}
     </div>
-  );
+  )
 }
 
 export default App;
