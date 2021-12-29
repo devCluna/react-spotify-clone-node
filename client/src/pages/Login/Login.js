d899f6f3dc9de547eafd63f7eb0a07b7529a7a81
Cristhian Luna <jobs.cluna@gmail.com>
2021-12-28 22:47:09 -0500

    fix(login): change typewritter pkg's <@devCluna>

--- client/src/pages/Login/Login.js
+++ client/src/pages/Login/Login.js
@@ -1,23 +1,21 @@
-import { useEffect, useRef, useState } from "react"
+import { useEffect, useState } from "react"
 import { AUTH_URL } from "../../lib/spotify"
 import { Description, DescriptionContainer, Developer, Disclaimer, DisclaimerButton, DisclaimerP, Github, Img, Linkedin, LoginBody, LoginButton, LoginContainer, LoginFooter, SocialMedia, Span, Title, Trademarks, Twitter, Video, Web } from "./LoginElements"
 import logo from '../../assets/logos/01_RGB/Spotify_Logo_RGB_White.png'
 import logo2 from '../../assets/logos/01_RGB/Spotify_Logo_RGB_Green.png'
 import video1 from '../../assets/videos/video1.mp4'
-import Typed from 'react-typed';
+import { Typewriter } from 'react-simple-typewriter'
 
 const Login = () => {
     const [focus, setFocus] = useState(false)
     const [toggleDisclaimer, setToggleDisclaimer] = useState(true)
+
+
     useEffect(()=>{
        document.title = "Spotify Client Login" 
     },[])
 
-
     return (
-        // <div>
-        //    <a href={AUTH_URL}>Login with Spotify</a> 
-        // </div>
         <LoginContainer>
             <LoginBody>
             <Video src={video1} autoPlay loop type="video/mp4"/>
@@ -33,21 +31,21 @@ const Login = () => {
                     <Title>
                         Listen without limits !!!
                     </Title>
-                    <Typed
-                        strings={[
-                            "Play millions of songs ad-free & on-demand",
-                            "Share",
-                            "Explore",
-                            "Listen to the songs you love",
-                            "Discover new music and podcasts",
-                            "Spotify is all the music you'll ever need"
-                        ]}
-                        shuffle={true}
-                        
-                        typeSpeed={40}
-                        backSpeed={50}
-                        loop 
-                    />
+                    <Typewriter
+                        words={[
+                        "Play millions of songs ad-free & on-demand","Share",
+                        "Explore",
+                        "Listen to the songs you love",
+                        "Discover new music and podcasts",
+                        "Spotify is all the music you'll ever need"]}
+                        loop
+                        cursor
+                        cursorStyle=' |'
+                        typeSpeed={70}
+                        deleteSpeed={50}
+                        delaySpeed={1000}
+                     />
+                
                     </Description>
                     <LoginButton href={AUTH_URL}>Login with Spotify</LoginButton>
                 </DescriptionContainer>
